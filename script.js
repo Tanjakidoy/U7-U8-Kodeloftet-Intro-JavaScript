69ba068b1c056d67af89e4ff8d5750b17eefd449
Tanjakidoy <tanjakidoy@gmail.com>
2024-09-30 21:35:20 +0200

    After Lesson

--- script.js
+++ script.js
@@ -1,50 +1,15 @@
+function loginCheck(username, currentHour, isLoggedIn) {
+  if (isLoggedIn) {
+      console.log("Login successful!");
+      console.log("Username: " + username);
+      console.log("Logged in at: " + currentHour + " hours.");
+  } else {
+      console.log("Login failed. Please try again.");
+  }
+}
 
-// A JavaScript Boolean represents one of two values: true or false //
+const username = "aliceWonder";
+let currentHour = new Date().getHours();
+let isLoggedIn = true;
 
-let x = 10;
-let y = 20;
-let isEqual = x === y;
-
-console.log('is the numbers equal');
-console.log(isEqual);
-
-let nameA = 'Lars';
-let nameB = 'Tanja';
-let isNamesEqual = nameA === nameB;
-
-console.log('is the names equal');
-console.log(isNamesEqual);
-
-let thisIsUndefined;
-console.log('This is undefined');
-console.log(thisIsUndefined);
-
-let doesNotYetHaveValue = null;
-console.log('The following does not yet have a value:');
-console.log(doesNotYetHaveValue);
-
-// A JavaScript array is a special variable, which can hold more than one value //
-
-let arrayA = ['Per', 'Pål', 'Espens', 'Martha'];
-
-console.log('This is a array (list)');
-console.log(arrayA);
-
-console.log('Accessing a single entry');
-console.log(arrayA[1]);
-
-let personA = {
-  name: 'Espen',
-  proffesion: 'Adventurer',
-  age: 18,
-  eyeColor: 'blue',
-};
-console.log('This is a collection of variables (Object)');
-console.log(personA);
-
-console.log('Accessing a single entry in an object');
-console.log(personA['proffesion']);
-console.log(personA.eyeColor);
-
-console.log(console);
-console.error('This is a error');
+loginCheck(username, currentHour, isLoggedIn);
