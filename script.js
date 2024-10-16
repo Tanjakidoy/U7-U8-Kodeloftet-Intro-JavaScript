a14543a76d51c9c30e28c1ace233d73214b0cec5
TanjaKodeloftet <tanjakidoy@gmail.com>
2024-10-16 14:30:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-function loginCheck(username, currentHour, isLoggedIn) {
-  if (isLoggedIn) {
-      console.log("Login successful!");
-      console.log("Username: " + username);
-      console.log("Logged in at: " + currentHour + " hours.");
-  } else {
-      console.log("Login failed. Please try again.");
+/* UNDERVISNING 7-8 : INTRO JAVASCRIPT */
+
+function loginCheck(username, currentHour, isLoggedIn) // Function er en blokk med kode som kan kalles ved å bruke funksjonsnavnet
+
+{
+  if (isLoggedIn) // Boolean representerer enten sann (true) eller usann (false) 
+
+/* The log() method writes (logs) a message to the console */    
+{ 
+    console.log("Login successful!");
+    console.log("Username: " + username);
+    console.log("Logged in at: " + currentHour + " hours.");
+  } 
+  
+  else {
+      console.log("Login failed. Please try again."); // Boolean representerer enten sann (true) eller usann (false)
   }
 }
 
+/* const er en variabel som ikke kan endres etter den er tildelt */
 const username = "Tanja";
+/* let er en variabel som kan endres etter den er tildelt verdi */
 let currentHour = new Date().getHours();
 let isLoggedIn = true;
 
