ddba4958d301a6481ff6756a8b04bddf02940a87
TanjaKodeloftet <tanjakidoy@gmail.com>
2024-10-16 15:17:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ const username = "Tanja"; // const er en variabel som ikke kan endres etter den 
 "Tanja" er verdien som blir lagret
 */
 
-let currentHour = new Date().getHours(); // "let" er ett nøkkelord for å lage en ny variabel. ("let" er en variabel som kan endres etter den er tildelt verdi.) 
-let isLoggedIn = true; // "let" er ett nøkkelord for å lage en ny variabel. ("let" er en variabel som kan endres etter den er tildelt verdi.) 
+let currentHour = new Date().getHours(); // ) 
+let isLoggedIn = true; // 
 
-
+/* "let" er ett nøkkelord for å lage en ny variabel. ("let" er en variabel som kan endres etter den er tildelt verdi.
+*/
